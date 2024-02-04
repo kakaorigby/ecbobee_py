de58777b46f4574bbd4ba290b32342ee6ad76070
kakaorigby <sc22h2s@leeds.ac.uk>
2024-02-04 14:27:10 +0000

    added user choice input and validation

--- posts_proj.py
+++ posts_proj.py
@@ -9,11 +9,24 @@ def get_random_posts():
     random.shuffle(posts)  # Shuffle posts list
     return posts[:10]  # Return the first 10 posts after shuffling
 
-def main():
-    posts = get_random_posts()
 
+def main():
+    print("This is the JSON Posts Project.")
+    print("Here is a list 10 random posts:")
+
+    posts = get_random_posts()
     for i in range(len(posts)):
         print(f"{i+1}. {posts[i]['title']}")
+
+    invalid = True
+    while(invalid):
+        print("\nWhich Post would you like to view? (Choose from 1-10)")
+        choice = int(input("Enter your choice: "))
+        if (choice < 11 and choice > 0):
+            invalid = False
+        else:
+            print("Please choose within the bounds 1-10!")
+    trueIndex = choice - 1
 
 if __name__ == "__main__":
     main()
